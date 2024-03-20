7a036afda679b8847203a57d650b91530a16f4a7
Tornelius Broadwater Jr <tkbjr01@icloud.com>
2024-03-20 18:31:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+const baseURL = "https://www.thecolorapi.com";
+const logoIcon = document.getElementById("theme-logo");
+const body = document.getElementById("body")
+let isLightMode = true;
+
+document.addEventListener("submit", (event) => {
+  event.preventDefault();
+  makeRequest();
+})
+
+document.addEventListener("click", (event) => {
+  const schemeDataSet = event.target.dataset.scheme;
+  if (schemeDataSet) {
+    navigator.clipboard.writeText(schemeDataSet);
+  }
+})
+
+document.getElementById("theme-btn").addEventListener("click", toggleTheme)
+
+function toggleTheme() {
+  isLightMode = !isLightMode
+  if (isLightMode) {
+    setLightMode()
+  } else {
+    setDarkMode()
+  }
+}
+
+function makeRequest() {
+  const hexValue = document.getElementById("color-input").value;
+  const colorSchemeValue = document.getElementById("color-scheme").value;
+  fetchColorScheme(hexValue, colorSchemeValue);
+}
+
+function setLightMode() {
+  logoIcon.classList.remove("fa-sun");
+  logoIcon.classList.add("fa-moon");
+  body.classList.remove("dark-mode")
+  body.classList.add("light-mode")
+}
+
+function setDarkMode() {
+  logoIcon.classList.remove("fa-moon")
+  logoIcon.classList.add("fa-sun")
+  body.classList.remove("light-mode")
+  body.classList.add("dark-mode")
+}
+
+function fetchColorScheme(hex, scheme) {
+  const cleanHex = hex.replaceAll("#", "");
+  fetch(`${baseURL}/scheme?hex=${cleanHex}&mode=${scheme}`)
+  .then(res => res.json())
+  .then(data => {
+    const retrievedColors = data["colors"].map( colorItem => {
+      return colorItem["hex"]["value"];
+    });
+    renderSchemeHTML(retrievedColors);
+  })
+}
+
+function renderSchemeHTML(hexArray) {
+  const schemeContainer = document.getElementById("scheme-container");  
+  const hexDisplayHTML = hexArray.map( item => {
+    return `
+    <div>
+      <div data-scheme="${item}" class="color-div" style="background-color:${item}"></div>
+      <p class="scheme-text">${item}</p>
+    </div>
+    `
+  }).join("");
+  schemeContainer.innerHTML = hexDisplayHTML;
+}
+
+makeRequest();
